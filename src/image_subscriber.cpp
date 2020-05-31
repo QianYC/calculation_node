ecbbd60b0ef7057116176f4efac1905e76e42f28
QianYC <1767855130@qq.com>
2020-05-31 17:12:38 +0800

    Update image_subscriber.cpp

--- src/image_subscriber.cpp
+++ src/image_subscriber.cpp
@@ -35,15 +35,17 @@ void sleep(int millisec) {
 }
 
 void subscribe_callback(const sensor_msgs::CompressedImage::ConstPtr &msg) {
-//    ROS_INFO("thread id %d", this_thread::get_id());
+    //读取一帧画面
     cv::Mat image = cv::imdecode(msg->data, CV_LOAD_IMAGE_COLOR);
     switch (state) {
+        //摆拍模式
         case STATIC:
             if (change_state) {
                 state = DYNAMIC;
                 change_state = false;
                 break;
             }
+            //目标检测
             faces = object_detect(image, detect_result);
             if (detect_result == HAVE_FACE) {
                 state = S_OBJECT_SELECTED;
@@ -52,24 +54,23 @@ void subscribe_callback(const sensor_msgs::CompressedImage::ConstPtr &msg) {
                 state = ROAMING;
             }
             break;
+        //目标已确定1
         case S_OBJECT_SELECTED:
             /**
-             * calculate expected position
-             * faces -> rect, mode(single/multiple)
+             * 进行构图
              */
             compose_result = calculate_expected_position(faces);
             state = S_COMPOSITION_SELECTED;
             break;
+        //构图已确定
         case S_COMPOSITION_SELECTED:
             /**
-             * calculate the error
-             * rect, mode, image -> success, direction
+             * 计算拍摄目标与期望位置的差距
              *
-             * try 5 times, go back to composition stage if still fail
+             * 出错5次以上，重新返回摆拍阶段
              */
             error_result = calculate_error(compose_result, image);
-            if (!error_result.success) {
-                //mode mismatch
+            if (!error_result.success) {              
                 if (adjust_fail == 5) {
                     adjust_fail = 0;
                     state = STATIC;
@@ -81,7 +82,6 @@ void subscribe_callback(const sensor_msgs::CompressedImage::ConstPtr &msg) {
                 state = S_POSITION_ADJUSTED;
                 break;
             } else {
-                //direction!=stop
                 if (adjust_fail == 5) {
                     adjust_fail = 0;
                     state = STATIC;
@@ -91,22 +91,26 @@ void subscribe_callback(const sensor_msgs::CompressedImage::ConstPtr &msg) {
                 }
                 break;
             }
+        //调整位置中
         case S_ADJUSTING:
             /**
-             * adjust position
+             * 发送运动指令给控制端
              */
             printf("moving %d\n", error_result.direction);
             motion_publisher.move(error_result.direction);
             state = S_COMPOSITION_SELECTED;
             break;
+        //位置已调整
         case S_POSITION_ADJUSTED:
             ROS_INFO("S_POSITION_ADJUSTED");
             motion_publisher.move(STOP);
             sleep(100);
+            //拍照
             take_photo(camera_client, compose_result);
             sleep(3000);
             state = STATIC;
             break;
+        //漫游模式
         case ROAMING:
             ROS_INFO("ROAMING");
             if (detect_result == FACE_TOO_SMALL) {
@@ -119,6 +123,7 @@ void subscribe_callback(const sensor_msgs::CompressedImage::ConstPtr &msg) {
             motion_publisher.move_for(motion, 1000);
             state = last_state;
             break;
+        //抓拍模式
         case DYNAMIC:
             ROS_INFO("DYNAMIC");
             if (change_state) {
@@ -135,6 +140,7 @@ void subscribe_callback(const sensor_msgs::CompressedImage::ConstPtr &msg) {
                 last_state = DYNAMIC;
                 break;
             }
+        //目标已确定2
         case D_OBJECT_SELECTED:
             ROS_INFO("D_OBJECT_SELECTED");
             sleep(100);
@@ -145,16 +151,6 @@ void subscribe_callback(const sensor_msgs::CompressedImage::ConstPtr &msg) {
             last_state = DYNAMIC;
             break;
     }
-    cv::line(image, cv::Point(0, 80), cv::Point(320, 80), cv::Scalar(0, 255, 0), 2);
-    cv::line(image, cv::Point(0, 160), cv::Point(320, 160), cv::Scalar(0, 255, 0), 2);
-    cv::line(image, cv::Point(106, 0), cv::Point(106, 240), cv::Scalar(0, 255, 0), 2);
-    cv::line(image, cv::Point(212, 0), cv::Point(212, 240), cv::Scalar(0, 255, 0), 2);
-    cv::rectangle(image, cv::Rect(66, 60, 80, 40), cv::Scalar(0, 0, 255), 2);
-    cv::rectangle(image, cv::Rect(172, 60, 80, 40), cv::Scalar(0, 0, 255), 2);
-    cv::rectangle(image, cv::Rect(66, 140, 80, 40), cv::Scalar(0, 0, 255), 2);
-    cv::rectangle(image, cv::Rect(172, 140, 80, 40), cv::Scalar(0, 0, 255), 2);
-    cv::imshow("subscriber", image);
-    cv::waitKey(1);
 }
 
 bool service_change_state(pi_robot::SrvTriggerRequest &request, pi_robot::SrvTriggerResponse &response) {
